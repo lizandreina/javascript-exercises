75297ba62d6666d70275769d21bc46286b2ba43c
lizandreina <156568298+lizandreina@users.noreply.github.com>
2024-06-03 15:36:49 +0200

    Pass all the removeFromArray tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(...theArgs) {
+    
+    let insideArray = theArgs[0];
+    let outsideArray = theArgs.slice(1);
+
+    const finalArray = [];
+
+    outer:
+    for (let i = 0; i < insideArray.length; i++) {
+        for (let j = 0; j < outsideArray.length; j++) {
+            if (insideArray[i] === outsideArray[j]) continue outer;
+        }
+    finalArray.push(insideArray[i]);
+    }
+    return finalArray;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+// Slice values between the array and the ones inside the array and create a variable for each of them
+// If values arguments in the array outside equal the ones in the inside, delete them from both
+// Remove any values missing in the array outside
