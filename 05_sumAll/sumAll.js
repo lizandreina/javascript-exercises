3e03787606256030ccd30592298725329fcc4639
lizandreina <156568298+lizandreina@users.noreply.github.com>
2024-06-04 22:26:13 +0200

    Pass all the sumAll tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(first, last) {
+    let finalSum = 0;
+    
+    if (first > 0 && last > 0 && typeof first === "number" && typeof last === "number") {
+        if (last > first) {
+            for (let i = first; i <= last; i++) {
+                finalSum += i;
+            }
+        } if (first > last) {
+            for (let i = last; i <= first; i++) {
+                finalSum += i;
+            }
+        }
+    } else {
+        return 'ERROR';
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
