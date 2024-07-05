e858d5946d78eead777d46556edd652b919524d5
lizandreina <156568298+lizandreina@users.noreply.github.com>
2024-07-05 19:10:14 +0200

    Pass all the calculator tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,28 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(item) {
+	return item.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(item) {
+  return item.reduce((a, b) => a * b);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(n) {
+	if (n === 0) return 1;
+  let result = Array.from({ length: n }, (v, i) => i + 1);
+  return result.reduce((a, b) => a * b);
+}
 
 // Do not edit below this line
 module.exports = {
