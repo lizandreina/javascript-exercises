79cb964dd046b1516cd6811f0f26ec983e3151d1
lizandreina <156568298+lizandreina@users.noreply.github.com>
2024-07-06 23:01:03 +0200

    Pass all the palindromes tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    let initial = input.replace(/[\s~`!@#$%^&*(){}\[\];:"'<,.>?\/\\|_+=-]/g, '').toLowerCase();
+    let reversed = input.replace(/[\s~`!@#$%^&*(){}\[\];:"'<,.>?\/\\|_+=-]/g, '').split('').reverse().join('').toLowerCase();
+    return initial === reversed;
 };
 
 // Do not edit below this line
