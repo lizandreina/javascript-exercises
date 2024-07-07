56b46a552688ca83e83907b6299c4e0cde3f17ca
lizandreina <156568298+lizandreina@users.noreply.github.com>
2024-07-07 14:40:13 +0200

    Pass all the fibonacci tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let converted = Number(number);
+    let sequence = [1, 1];
+    for(let i = 2; i <= converted; i++){
+        sequence.push(sequence[i-1] + sequence[i-2]);
+    }
+    if (converted === 0) return 0;
+    if (converted < 0) return 'OOPS';
+    return sequence[converted - 1];
 };
 
 // Do not edit below this line
