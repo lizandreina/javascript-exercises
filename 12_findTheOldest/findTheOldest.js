8b759799fc0a5120fd177281ef96bc39c3eb5d69
lizandreina <156568298+lizandreina@users.noreply.github.com>
2024-07-08 17:55:57 +0200

    Pass all the findTheOldest tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let currentYear = new Date().getFullYear();
+    people.map(object => {
+        if (!object.yearOfDeath) {object.yearOfDeath = currentYear};
+});
+    people.sort((a, b) => a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth ? -1 : 1);
+    return people[0];
 };
 
 // Do not edit below this line
